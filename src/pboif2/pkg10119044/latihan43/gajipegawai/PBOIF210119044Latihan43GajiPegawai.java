e0d96991f3f4dc0f85d9e2e6ad9d4f896ea20c44
ASUS <ASUS@ASUS-X541U>
2020-11-12 09:20:25 +0700

    Program ini bertujuan menampilkan gaji pegawai

---
+++ src/pboif2/pkg10119044/latihan43/gajipegawai/PBOIF210119044Latihan43GajiPegawai.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119044.latihan43.gajipegawai;
+
+/**
+ *
+ * @author 
+ * NAMA     : Yohana Sri Rejeki
+ * KELAS    : IF2
+ * NIM      : 10119044
+ * Deskripsi Program : Program ini bertujuan menampilkan gaji pegawai
+ */
+public class PBOIF210119044Latihan43GajiPegawai {
+    public static void main(String[] args) {
+        GajiPegawai objgaji = new GajiPegawai();
+        objgaji.tampilData("Rizki Adam Kurniawan", "Jalan Semar dlm 4 No 72/66", 300000, 250000, 4500000, 5050000);
+    }
+    
+}
